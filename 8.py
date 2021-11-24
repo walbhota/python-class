36244343dd954f56f162ede552781875822aa642
j8 <walbhota@gmail.com>
2021-11-24 02:02:23 +0100

    code for class

---
+++ 8.py
@@ -0,0 +1,14 @@
+class student:
+    pass
+
+
+mashrur = student()
+john = student()
+
+mashrur.first_name = "mashrur"
+mashrur.last_name = "hossain"
+john.first_name = "john"
+john.last_name = "doe"
+
+print(mashrur.first_name, mashrur.last_name)
+print(john.first_name, john.last_name)
